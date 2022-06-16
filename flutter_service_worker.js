811a669bdc6be6507b4e32e4937224a2af5d2c55
Barış Hakan Sarıağaç <barishakansariagac@gmail.com>
2022-06-16 11:26:45 +0300

    price update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -216,9 +216,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "ac3b01d1e248584384958a1e7e85a582",
-"/": "ac3b01d1e248584384958a1e7e85a582",
-"main.dart.js": "a2dc6f8356531209d8c82bb7b4d7f724",
+"index.html": "7ea55aa6ef1dde30a62a668c9a9c805e",
+"/": "7ea55aa6ef1dde30a62a668c9a9c805e",
+"main.dart.js": "39e0df4ca9790132930503d626d777d8",
 "manifest.json": "cca2415df12cddd406fafeb90116887a",
 "version.json": "939180a8f5ac8a0fbd34c74b11a7305a"
 };
